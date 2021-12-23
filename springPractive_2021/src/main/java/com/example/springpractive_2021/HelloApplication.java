58818ed8ac346fb970084376bc28e35bf376da40
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2021-12-23 20:26:07 +0900

    feat : 기존 소스코드 삭제 프로젝트 재시작

--- springPractive_2021/src/main/java/com/example/springpractive_2021/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.springpractive_2021;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
